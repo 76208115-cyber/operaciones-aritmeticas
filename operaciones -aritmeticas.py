430278036bdcedc03f584ea5ee8df8324af34f4d
Grecia <76208115@continental.edu.pe>
2026-01-06 18:39:12 -0500

    se implemento la suma de los numeros

--- operaciones -aritmeticas.py
+++ operaciones -aritmeticas.py
@@ -1,1 +1,4 @@
-print ("Hello, wordl")
+numero1 = int(input("ingrese el primer numero: "))
+numero2 = int(input("ingrese el segundo numero: "))
+suma = numero1 + numero2
+print("la suma es: ", suma)
